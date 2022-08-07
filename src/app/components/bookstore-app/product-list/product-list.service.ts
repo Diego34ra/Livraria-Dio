9e6afd826b27ae8050abb888339f7f3305a26de6
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-08-07 09:36:32 -0300

    commit inicial
    Implementação da aplicação

---
+++ src/app/components/bookstore-app/product-list/product-list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Book } from './model/books';
+
+export const books: Book[] = [
+  { id: '1', name: 'Book1' , price: 10, quantity: 1, category: "Acao", img:"img1"},
+  { id: '2', name: 'Book2' , price: 10, quantity: 1, category: "Acao", img:"img2"},
+  { id: '3', name: 'Book3' , price: 10, quantity: 1, category: "Acao", img:"img3"},
+
+];
+
+@Injectable()
+export class BooksService {
+
+  private url = 'https://localhost:44382/api/bookstore';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  constructor( private http: HttpClient ) { }
+
+
+  getBooks() {
+    return this.http.get(this.url)
+
+
+  }
+}
